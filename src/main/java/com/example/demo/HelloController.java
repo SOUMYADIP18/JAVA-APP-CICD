cb7d6b10bfbac5e7cb29f091b1b68a9c5246c75e
SOUMYADIP18 <soumyadipsamanta903@gmail.com>
2026-03-18 16:54:14 +0530

    Updated website message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "DevOps Java App Running! 🚀";
+        return "DevOps Java App is LIVE on AWS! V2 😎";
     }
 }
